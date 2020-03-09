eac27287462b2439210b94e244f041ca651d6660
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-09 07:41:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,10 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |k, v|
+      self.send(("#{k}="), v)
+    end
+    self
   end
 
   def self.all
