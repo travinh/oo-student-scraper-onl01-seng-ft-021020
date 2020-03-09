3d517220aa750fb047c49741940b6481e7a1de2d
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-09 07:36:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,20 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    
+    student_array = []
+    
+    doc.css("div.student-card").each do |student|
+      name = 
+    end
+    
+    
     
   end
 
